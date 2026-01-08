c2dfab2cd8cafd19e64ea6b57e2679c45213fdf0
Alex Mickelson <alexmickelson96@gmail.com>
2026-01-07 20:31:27 -0700

    flake stuff

--- run.sh
+++ run.sh
@@ -42,8 +42,8 @@ cd scripts
     ./4-configure-bwapi.sh
 cd ..
 
-echo "Building RustBot..."
-build-rustbot-debug
+# echo "Building RustBot..."
+# build-rustbot-debug
 echo "Starting RustBot..."
 cd "$SCRIPT_DIR/rustbot"
 
